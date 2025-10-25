44c015fba65430517f0df2c0efc951b968a132c6
Julius Marminge <julius0216@outlook.com>
2025-10-24 17:46:28 -0700

    init

---
+++ src/routes/__root.tsx
@@ -0,0 +1,47 @@
+/// <reference types="vite/client" />
+import * as React from 'react'
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+import {
+  HeadContent,
+  Link,
+  Outlet,
+  Scripts,
+  createRootRoute,
+} from '@tanstack/react-router'
+import { createServerFn } from '@tanstack/react-start'
+
+const readContext = createServerFn().handler((args) => {
+  console.log("[serverFn] args.context", args.context);
+})
+
+export const Route = createRootRoute({
+  loader: () => readContext(),
+  component: RootComponent,
+})
+
+function RootComponent() {
+  return (
+    <RootDocument>
+      <Outlet />
+    </RootDocument>
+  )
+}
+
+function RootDocument({ children }: { children: React.ReactNode }) {
+  return (
+    <html>
+      <head>
+        <HeadContent />
+      </head>
+      <body>
+        <div>
+          <Link to="/">Index</Link>
+        </div>
+
+        {children}
+        <TanStackRouterDevtools position="bottom-right" />
+        <Scripts />
+      </body>
+    </html>
+  )
+}
